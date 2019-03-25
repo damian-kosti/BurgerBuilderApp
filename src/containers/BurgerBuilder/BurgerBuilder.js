e5f75ebfee3b0fc9020d316026185b33343842a5
Damian Kostrzewa <damian.kostrzewa@printsoftnt.pl>
2019-03-25 17:05:34 +0100

    Added ingredients quantity controls and dynamic burger view

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,15 +1,32 @@
 import React, { Component } from 'react';
 
 import Burger from '../../components/Burger/Burger';
-import Wrapper from '../../hoc/Wrapper';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
+
+//import Wrapper from '../../hoc/Wrapper';
 
 class BurgerBuilder extends Component {
+    // constructor(props) {
+    //     super(props);
+    //     this.state = { ... } 
+    // }
+    
+    state = {
+        ingredients: {
+            salad: 0,
+            bacon: 0,
+            cheese: 0,
+            meat: 0
+        }
+    }
+
+
     render() {
         return (
-            <Wrapper>
-                <Burger />
-                <div>Build Controls</div>
-            </Wrapper>
+            <>
+                <Burger ingredients={this.state.ingredients}/>
+                <BuildControls />
+            </>
         );
     }
 }
